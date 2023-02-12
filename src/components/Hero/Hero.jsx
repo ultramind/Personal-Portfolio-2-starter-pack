f6114a9f0a4a31a82368f3ea011c26f6b8a0676b
ultragrace <udechukwuakachukwu.com>
2023-02-12 20:46:59 +0100

    Added animation on Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,45 +1,76 @@
 import React from "react";
 import css from "./hero.module.scss";
+import { motion } from "framer-motion";
+import { fadeIn, slideIn, staggerContainer } from "../../utils/motion";
 
 const Hero = () => {
   return (
-    <div className={`paddings innerWidth ${css.wrapper}`}>
-      <div className={css.container}>
+    <section className={`paddings ${css.wrapper}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`innerWidth ${css.container}`}
+      >
         <div className={css.upperElements}>
-          <span className="primaryText">
+          <motion.span
+            variants={fadeIn("right", "tween", 0.2, 1)}
+            className="primaryText"
+          >
             Hey There, <br /> I'm AkaGod
-          </span>
-          <span className="secondaryText">
+          </motion.span>
+          <motion.span
+            variants={fadeIn("left", "tween", 0.2, 1)}
+            className="secondaryText"
+          >
             i design and develope beautiful <br /> things, i love what i do
-          </span>
+          </motion.span>
         </div>
 
         {/* person image */}
 
-        <div className={css.person}>
-          <img src="./person.png" alt="" />
-        </div>
+        <motion.div
+          variants={fadeIn("up", "tween", 0.2, 1)}
+          className={css.person}
+        >
+          <motion.img
+            variants={slideIn("up", "tween", 0.2, 1)}
+            src="./person.png"
+            alt=""
+          />
+        </motion.div>
 
         {/* Email */}
-        <a href="mailto:udechukwuakachukwu@gmail.com" className={css.email}>
+        <motion.a
+          variants={fadeIn("right", "tween", 0.2, 1)}
+          href="mailto:udechukwuakachukwu@gmail.com"
+          className={css.email}
+        >
           udechukwuakachukwu@gmail.com
-        </a>
+        </motion.a>
         <div className={css.lowerElements}>
-          <div className={css.experience}>
+          <motion.div
+            variants={fadeIn("right", "tween", 0.2, 1)}
+            className={css.experience}
+          >
             <div className="primaryText">5</div>
             <div className="secondaryText">
               <div>Years</div>
               <div>Experience</div>
             </div>
-          </div>
-          <div className={css.certificate}>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", "tween", 0.2, 1)}
+            className={css.certificate}
+          >
             <img src="./certificate.png" alt="" />
             <span>CERIFIED PROFESSIONAL</span>
             <span>FULLSTACK DEVELOPER</span>
-          </div>
+          </motion.div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </section>
   );
 };
 
